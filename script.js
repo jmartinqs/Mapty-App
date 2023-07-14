e330db0ff52fcf2d3a81dc695cc8d405a5330941
Martin Quevedo <martinqs@live.com.ar>
2023-07-14 11:31:51 -0300

    Implementing API

--- script.js
+++ script.js
@@ -11,72 +11,16 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// let passengersBoarded = 18;
+//////////// Geolocation API /////////
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
 
-// if (passengersBoarded <= 10) {
-//   passengersBoarded += 10;
-// } else {
-//   passengersBoarded -= passengersBoarded - 10;
-// }
-
-// for (let i = 0; i < 5; i++) {
-//     console.log('Oh hey!');
-// }
-
-// const passengers = [
-//     'Will Alexander',
-//     'Sarah Kate',
-//     'Audrey Simon',
-//     'Tau Perkington'
-// ];
-
-// let passengersBoarded = 10;
-
-// for (let i in passengers) {
-//     passengersBoarded++;
-// }
-
-// console.log(passengersBoarded);
-
-// const guests = [
-//   {
-//     name: 'Will Alexander',
-//     vip: false,
-//   },
-//   {
-//     name: 'Sarah Kate',
-//     vip: true,
-//   },
-//   {
-//     name: 'Audrey Simon',
-//     vip: true,
-//   },
-//   {
-//     name: 'Tau Perkington',
-//     vip: false,
-//   },
-// ];
-
-// let vips = 0;
-// let normies = 0;
-
-// for (let guest of guests) {
-//   if (guest.vip) {
-//     vips++;
-//   }
-// }
-
-// console.log(vips);
-
-// let guestsSeated = 5;
-
-// let seatsRemaining = 10;
-// let guestsRemaining = 7;
-
-// while (seatsRemaining > 0 && guestsRemaining > 0) {
-//     guestsSeated++;
-//     seatsRemaining--;
-//     guestsRemaining--;
-// }
-
-// console.log(guestsSeated);
+    function () {
+      alert('Could not get your position!');
+    }
+  );
